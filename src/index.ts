3fddb0ae8e3f19975c8847dcf9dfcde52d84be77
m.dolgikh <m.dolgikh@qiwi.com>
2018-10-02 16:15:47 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import * as Consul from 'consul'
+
+export interface IConnectionParams {
+    post: string
+    host: string
+}
+
+export interface IConsulService {
+    getConnectionParams(serviceName: string): Promise<IConnectionParams>
+}
+
+export interface IConnectionParams {
+    host: string;
+    port: string;
+}
+
+export interface IConsulWatchOptions {
+    service: string
+    passing: boolean
+}
+
+export interface IEntryPoint {
+    Service: {
+        Address: string
+        Port: string
+    }
+}
+
+export default class ConsulService implements IConsulService {
+    protected _consul: any
+    protected _instances: any = {}
+    protected _instancesWatcher: any
+
+    constructor(
+        consulConnectionParams: IConnectionParams
+    ) {
+        this._consul = new Consul({
+            host: consulConnectionParams.host,
+            port: consulConnectionParams.port.toString()
+        })
+    }
+
+    async init(serviceName: string): Promise<void> {
+        let resolveInit: (value?: void | PromiseLike<void>) => void
+        let rejectInit: (reason?: any) => void
+
+        const promise = new Promise<void>((resolve, reject) => {
+            resolveInit = resolve
+            rejectInit = reject
+        })
+
+        this._instancesWatcher = this._consul.watch({
+            method: this._consul.health.service,
+            options: {
+                service: serviceName,
+                passing: true
+            } as IConsulWatchOptions,
+            backoffMax: 10000
+        })
+        .on('change', (data: IEntryPoint[]) => {
+            this._instances[serviceName] = []
+            data.forEach((entryPoint: IEntryPoint) => {
+                this._instances[serviceName].push({
+                    host: entryPoint.Service.Address,
+                    port: entryPoint.Service.Port
+                })
+            })
+            if (this._instances[serviceName]) {
+                resolveInit()
+            }
+        })
+        .on('error', (err: Error) => {
+            console.error('Consul client error:', err)
+            rejectInit()
+        })
+        return promise
+    }
+
+    async getConnectionParams(serviceName: string): Promise<IConnectionParams> {
+        if (!this._instances[serviceName]) {
+            await this.init(serviceName)
+        }
+        const index = Math.floor(Math.random() * this._instances[serviceName].length)
+        return this._instances[serviceName][index]
+    }
+}
