5e913aaca4a23b65fde4cf43d8954d17d4742c65
Anton Golub <antongolub@antongolub.com>
2021-10-01 13:02:14 +0300

    feat: rebuild watcher instance if it hangs

--- src/main/ts/cxt.ts
+++ src/main/ts/cxt.ts
@@ -3,11 +3,13 @@
 import { ICxt, ILibConfig } from './interface'
 import * as Consul from 'consul'
 import { createLogger } from './logger'
+import { DEFAULT_TIMEOUT } from './defaults'
 
 export const createContext = (cxt: ILibConfig = {}): ICxt => {
   return {
     Consul: cxt.Consul || Consul,
     Promise: cxt.Promise || Promise,
-    logger: createLogger(cxt.logger || console)
+    logger: createLogger(cxt.logger || console),
+    timeout: cxt.timeout || DEFAULT_TIMEOUT
   }
 }
---
+++ src/main/ts/defaults.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_TIMEOUT = 10000
+
+export const BACKOFF_MAX = 20000
+
+export const WATCH_ERROR_LIMIT = 20
